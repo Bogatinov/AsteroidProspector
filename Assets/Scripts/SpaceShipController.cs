fbda8ce13e08f81a01d70f94a4a11e555866e685
unknown-user <unknown-user@fe80::8458:909:6bd2:b8ec%13>
2014-04-12 12:04:22 +0200

    sfg

--- Assets/Scripts/SpaceShipController.cs
+++ Assets/Scripts/SpaceShipController.cs
@@ -6,11 +6,12 @@ public class SpaceShipController : MonoBehaviour
 
 		private Vector3 moveDirection;
 		public float moveSpeed = 2;
+		public float turnSpeed;
 
 		// Use this for initialization
 		void Start ()
 		{
-	
+				moveDirection = Vector3.right;
 		}
 	
 		// Update is called once per frame
@@ -27,5 +28,10 @@ public class SpaceShipController : MonoBehaviour
 						moveDirection.z = 0; 
 						moveDirection.Normalize ();
 				}
+				float targetAngle = Mathf.Atan2 (moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+				transform.rotation = 
+			Quaternion.Slerp (transform.rotation, 
+			                 Quaternion.Euler (0, 0, targetAngle), 
+			                 turnSpeed * Time.deltaTime);
 		}
 }
