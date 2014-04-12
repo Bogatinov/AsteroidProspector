beefabfc81e4a489704b977f189ebd3a8f345a68
unknown-user <unknown-user@fe80::8458:909:6bd2:b8ec%13>
2014-04-12 13:48:55 +0200

    se novo

---
+++ Assets/Scripts/AsteroidContoller.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidContoller : MonoBehaviour
+{
+
+		// Use this for initialization
+		void Start ()
+		{
+				transform.Rotate (Vector3.forward * -90);
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				//rigidbody2D.AddTorque (5);
+				transform.Rotate (Vector3.forward * -90 * Time.deltaTime);
+				
+		}
+}
