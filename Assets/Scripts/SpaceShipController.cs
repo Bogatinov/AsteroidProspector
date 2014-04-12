8facbd36fa95fc03a0453af01b535796881429c1
unknown-user <unknown-user@fe80::8458:909:6bd2:b8ec%13>
2014-04-12 11:46:41 +0200

    new script

---
+++ Assets/Scripts/SpaceShipController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceShipController : MonoBehaviour
+{
+
+		private Vector3 moveDirection;
+		public float moveSpeed = 2;
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				// 1
+				Vector3 currentPosition = transform.position;
+				// 2
+				if (Input.GetButton ("Fire1")) {
+						// 3
+						Vector3 moveToward = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+						// 4
+						moveDirection = moveToward - currentPosition;
+						moveDirection.z = 0; 
+						moveDirection.Normalize ();
+				}
+		}
+}
